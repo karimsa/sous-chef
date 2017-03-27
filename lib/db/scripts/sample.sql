9c00218274b7048ccb4b6c8c1ad468bd064a2e71
Karim Alibhai <karim@alibhai.co>
2017-03-27 15:02:51 -0400

    Add comments for inserts

--- lib/db/scripts/sample.sql
+++ lib/db/scripts/sample.sql
@@ -1,5 +1,7 @@
 -- sample data for testing
 
+-- the field "uid" is first, but is not mentioned in the INSERT
+-- because it is auto incrementing
 INSERT INTO AppUser(name, email, password, type) VALUES
   ('Admin', 'admin@gmail.com', 'test', 'admin'),
   ('Chef', 'chef@gmail.com', 'test', 'chef'),
@@ -8,6 +10,8 @@ INSERT INTO AppUser(name, email, password, type) VALUES
 INSERT INTO Item VALUES
   ('Apples', 3, 'Fruit', 5, 2, 5, 2);
 
+-- the field "id" is first, but is not mentioned in the INSERT
+-- because it is auto incrementing
 INSERT INTO Recipe(name, description, category) VALUES
   ('Cheese Burger', 'Classic American burger, but with cheese.', 'Fast Food'),
   ('Chicken Souvlaki', 'Slow roasted chicken with garlic and stuff.', 'Greek'),
